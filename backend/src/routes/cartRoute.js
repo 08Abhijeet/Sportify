7958598920e36f81eb198dfd1740301f98dffcd0
AbHiJeet KaDam <145742954+08Abhijeet@users.noreply.github.com>
2025-01-03 21:35:27 +0530

    Delete backend directory

--- backend/src/routes/cartRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { addToCart,updateCart,getUserCart } from "../controllers/cartController.js";
-const cartRouter = express.Router();
-
-cartRouter.post("/get", getUserCart);
-cartRouter.post("/add", addToCart);
-cartRouter.post("/update", updateCart);
-
-export default cartRouter;
